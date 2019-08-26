c2aa23aa9a4d2eb72adeb4b621eaa042af7aea13
xiaokiwi <milk0730@gmail.com>
2019-08-26 19:28:54 -0400

    Modified the homepage swiper effect to slide.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
     direction: 'horizontal',
     loop: true,
     keyboard: true,
-    mousewheel: true,
+    mousewheel: false,
     scrollbar: false,
     navigation: true,
     pagination: {
@@ -30,7 +30,7 @@ export class HomeComponent implements OnInit {
       clickable: true
     },
     autoplay: true,
-    effect: 'coverflow',
+    effect: 'slide',
     slidesPerView: 'auto',
     spaceBetween: 20,
     observer: true,
