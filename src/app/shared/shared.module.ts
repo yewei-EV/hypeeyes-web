5aec6fe3b793d4a3eee482c2257fbd5a5c28a88c
Pei Geng <gengpei@tianbaotravel.com>
2019-09-03 19:47:37 -0400

    fix ui

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   exports: [
     CommonModule,
     TranslateModule,
+    NgbPopoverModule,
   ],
 })
 export class SharedModule {
