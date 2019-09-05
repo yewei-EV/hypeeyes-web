cd01cc9c2a10f83e882d0d6f274b129662e5d64c
Pei Geng <gengpei@tianbaotravel.com>
2019-09-05 16:14:19 -0400

    change menu, change iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
   title = 'hypeeyes-web';
+  offsetHeight = 0;
+  constructor() {
+    document.domain = 'localhost';
+  }
+
+  ngAfterViewChecked(): void {
+    const offsetHeight = document.body.offsetHeight;
+    if (this.offsetHeight < offsetHeight) {
+      this.offsetHeight = offsetHeight;
+    }
+    parent.postMessage({type: 'height', height: this.offsetHeight}, '*');
+    console.log(this.offsetHeight);
+  }
+
 }
