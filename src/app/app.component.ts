ec0575cb6f12f80af202de536ed651a941faf60f
Pei Geng <gengpei@tianbaotravel.com>
2019-09-30 18:26:41 -0400

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewChecked {
   title = 'hypeeyes-web';
 
-  ngOnInit(): void {
+  ngAfterViewChecked(): void {
     const height = document.body.scrollHeight;
     if (location.origin === 'http://127.0.0.1:4200') {
       window.parent.postMessage(height, 'http://127.0.0.1:4567');
     } else if (location.origin === 'http://localhost:4200') {
       window.parent.postMessage(height, 'http://localhost:4567');
     } else {
-      window.parent.postMessage(height, location.protocol + '//hypeeyes.com');
+      window.parent.postMessage(height, location.origin);
     }
   }
 }
