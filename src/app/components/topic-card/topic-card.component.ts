1b51e2b251bb0d33b18be79038ef9148f88e37b0
gengpei <gengpei@tianbaotravel.com>
2019-12-19 13:18:27 -0500

    adapter with phones

--- src/app/components/topic-card/topic-card.component.ts
+++ src/app/components/topic-card/topic-card.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { Config, Topic } from '../../shared/models';
 import { ConfigService } from '../../shared/service/config.service';
 
 class Style {
   width: string;
+  aspectRatio: string;
 }
 
 @Component({
@@ -15,10 +16,11 @@ export class TopicCardComponent implements OnInit {
 
   @Input() topic: Topic;
   @Input() styleType: number;
+  @HostBinding('style.width') width: string;
   style: Style;
   private styles: Style[] = [
-    {width: '390px'},
-    {width: '288px'}
+    {width: '32.5%', aspectRatio: '66.6%'},
+    {width: '24%', aspectRatio: '90%'}
   ];
 
   config: Config;
@@ -26,6 +28,7 @@ export class TopicCardComponent implements OnInit {
 
   ngOnInit() {
     this.style = this.styles[this.styleType];
+    this.width = this.style.width;
     this.configService.getConfig().subscribe(config => this.config = config);
   }
 
