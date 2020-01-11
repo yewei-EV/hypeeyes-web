8f6bd07286b6cdc88e89af095c34301c10f144a5
xiaokiwi <milk0730@gmail.com>
2020-01-11 18:53:32 -0500

    improve ui of homepage

--- src/app/shared/constant.ts
+++ src/app/shared/constant.ts
@@ -5,7 +5,7 @@ export class Constant {
   static topicListInfoMap: Map<string, TopicListInfo> = new Map([
     ['潮目秘籍', {line: 1, itemNumberPerLine: 3, maxItems: 3}],
     ['潮流百科', {line: 1, itemNumberPerLine: 3, maxItems: 3}],
-    ['美图细赏', {line: 1, itemNumberPerLine: 4, maxItems: 4}],
+    ['美图细赏', {line: 1, itemNumberPerLine: 3, maxItems: 3}],
     ['潮目社区', {line: 1, itemNumberPerLine: 4, maxItems: 4}],
   ]);
 }
